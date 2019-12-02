f2babe87ea1c834475c3867353fb68c7e9e3cdc8
CoderCallender <42542281+CoderCallender@users.noreply.github.com>
2019-12-02 15:36:20 +0000

    Update humidity_log.py
    Added debug logging and formatted the output

--- humidity_log.py
+++ humidity_log.py
@@ -1,6 +1,7 @@
 import thingspeak
 import time
 import Adafruit_DHT
+import logging
  
 channel_id = 884199 # PUT CHANNEL ID HERE
 write_key  = '11B4V75M25DQ5U8E' # PUT YOUR WRITE KEY HERE
@@ -11,20 +12,26 @@ sensor = Adafruit_DHT.DHT22
 def measure(channel):
     try:
         humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+        format(humidity, '.2f')
+        format(temperature, '.2f')
+       # print ('temp = 
         # write
         response = channel.update({'field1': temperature, 'field2': humidity})
         
         # read
         read = channel.get({})
         #print("Read:", read)
-        print("Update Success!")
+        logging.info("Update Success!")
         
     except:
-        print("connection failed")
+        #print("connection failed")
+        logging.error('connection failed')
  
  
 if __name__ == "__main__":
     #channel = thingspeak.Channel(id=channel_id, write_key=write_key, api_key=read_key)
+    #logging.basicConfig(format='%(asctime)s - %(message)s')
+    logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(message)s')
     channel = thingspeak.Channel(id=channel_id, api_key=write_key)
     while True:
         measure(channel)
